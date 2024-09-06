a9f20da64cb48684e938af57a62821406422ed0e
GiSeong Lee <cj8928@gmail.com>
2024-09-06 17:07:34 +0900

    update: tanstack query 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import "./reset.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./shared/Router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   </StrictMode>
 );
