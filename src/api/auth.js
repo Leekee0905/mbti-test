cefe721ba244e62737a37fea20f513fe0febfef8
GiSeong Lee <cj8928@gmail.com>
2024-09-07 15:22:54 +0900

    update: login, user정보 가져오는 api 생성

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,8 +9,27 @@ export const register = async (userData) => {
   }
 };
 
-export const login = async (userData) => {};
+export const login = async (userData) => {
+  try {
+    const response = await authApi.post("/login", userData);
+    return response.data;
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+};
 
-export const getUserProfile = async (token) => {};
+export const getUserProfile = async (token) => {
+  try {
+    const response = await authApi.get("/user", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    alert(error.response.data.message);
+  }
+};
 
 export const updateProfile = async (formData) => {};
