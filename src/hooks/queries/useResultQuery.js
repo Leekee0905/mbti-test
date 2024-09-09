7a2636ecaf8648c36022ea9644eed314249ed152
GiSeong Lee <cj8928@gmail.com>
2024-09-09 17:13:13 +0900

    add: result query 커스텀훅 생성

---
+++ src/hooks/queries/useResultQuery.js
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
+import { getTestResults } from "../../api/testResult";
+
+export const useResultQuery = () => {
+  return useQuery({
+    queryKey: ["testResult"],
+    queryFn: getTestResults,
+  });
+};
