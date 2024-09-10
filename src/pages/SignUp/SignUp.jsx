b4c913a234220a2f8ad1e0eafc4c4bec17921930
GiSeong Lee <cj8928@gmail.com>
2024-09-10 10:58:41 +0900

    style: 회원가입 스타일링

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,7 +3,10 @@ import AuthForm from "../../components/AuthForm";
 const SignUp = () => {
   return (
     <div className="w-1/2 h-1/2 mt-10 rounded-lg flex justify-center items-center bg-white">
-      <AuthForm mode={"signup"} />
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-700 my-10">회원가입</h1>
+        <AuthForm mode={"signup"} />
+      </div>
     </div>
   );
 };
