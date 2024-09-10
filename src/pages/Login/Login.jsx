c3fc1ddf1187533f7b7a51ded8a14a224b7db428
GiSeong Lee <cj8928@gmail.com>
2024-09-10 10:57:58 +0900

    style: 로그인 페이지 스타일링

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,12 +4,15 @@ import AuthForm from "../../components/AuthForm";
 const Login = () => {
   return (
     <div className="w-1/2 h-1/2 mt-10 rounded-lg flex justify-center items-center bg-white">
-      <div>
-        <h1>로그인</h1>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold text-gray-700 my-10">로그인</h1>
         <AuthForm mode="login" />
         <div>
-          <p>
-            계정이 없으신가요? <Link to="/signup">회원가입</Link>
+          <p className="mb-5">
+            계정이 없으신가요?{" "}
+            <Link to="/signup" className="text-red-500 font-bold">
+              회원가입
+            </Link>
           </p>
         </div>
       </div>
